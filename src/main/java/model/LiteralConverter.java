09352fbfb44e3a28f173e45b38d490e30f19aa52
Andrea Brighi <andrea.brighi8@studio.unibo.it>
2025-10-15 02:23:45 +0200

    feat(Environment): Create a utility to convert the model to Literal

---
+++ src/main/java/model/LiteralConverter.java
@@ -0,0 +1,46 @@
+package model;
+
+import jason.asSyntax.Literal;
+import model.math.Point;
+import model.roadElement.RoadElement;
+import model.roadElement.Sign;
+import model.roadElement.TrafficLight;
+
+public class LiteralConverter {
+
+    public static String NEXT_STREET_ELEMENT_BASE = "next_street_element(TYPE, X, Y)";
+    public static String POSITION_BASE = "position(X, Y)";
+    public static String CURRENT_SPEED_BASE = "currentSpeed(CS)";
+    public static String SPEED_LIMIT_BASE = "speedLimit(LIMIT)";
+    public static String SIGN_TYPE_BASE = "sign(TYPE, X, Y)";
+    public static String TRAFFIC_LIGHT_BASE = "traffic_light(STATE, X, Y)";
+
+    public static Literal positionToLiteral(Point p) {
+        return Literal.parseLiteral(POSITION_BASE.replace("X", String.valueOf(p.getX()))
+                .replace("Y", String.valueOf(p.getY())));
+    }
+
+    public static Literal currentSpeedToLiteral(double speed) {
+        return Literal.parseLiteral(CURRENT_SPEED_BASE.replace("CS", String.valueOf(speed)));
+    }
+
+    public static Literal speedLimitToLiteral(int speedLimit) {
+        return Literal.parseLiteral(SPEED_LIMIT_BASE.replace("LIMIT", String.valueOf(speedLimit)));
+    }
+
+    public static Literal signTypeToLiteral(Sign.SignType signType) {
+        return Literal.parseLiteral(SIGN_TYPE_BASE.replace("TYPE", signType.toString().toLowerCase()));
+    }
+
+    public static Literal signToLiteral(Sign s) {
+        return Literal.parseLiteral(SIGN_TYPE_BASE.replace("TYPE", s.getType().toString().toLowerCase())
+                .replace("X", String.valueOf(s.getPosition().getX()))
+                .replace("Y", String.valueOf(s.getPosition().getY())));
+    }
+
+    public static Literal trafficLightToLiteral(TrafficLight tl) {
+        return Literal.parseLiteral(TRAFFIC_LIGHT_BASE.replace("STATE", tl.getState().toString().toLowerCase())
+                .replace("X", String.valueOf(tl.getPosition().getX()))
+                .replace("Y", String.valueOf(tl.getPosition().getY())));
+    }
+}
